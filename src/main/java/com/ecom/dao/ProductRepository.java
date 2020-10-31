6c056e8cb693b97a55c82871d88f6e81cb5d1967
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2020-10-31 20:13:00 +0530

    2.0 - enable cross origin requests

--- src/main/java/com/ecom/dao/ProductRepository.java
+++ src/main/java/com/ecom/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.ecom.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecom.entity.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
