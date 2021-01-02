0add54ab8add6ec9cd2cbf7af562df9c80dc704c
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2021-01-03 00:29:04 +0530

    3.0 - Security - login/logout functionality added

---
+++ angular-ecom/src/app/config/app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3aex4tOs2hZpqo5d6',
+        issuer: 'https://dev-7562370.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email'] 
+    }
+}
