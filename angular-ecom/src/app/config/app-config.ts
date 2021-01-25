22d90ce16362f5b85e873e28837ebb2abcd14839
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2021-01-25 19:54:48 +0530

    Added schema diagram

--- angular-ecom/src/app/config/app-config.ts
+++ angular-ecom/src/app/config/app-config.ts
@@ -1,7 +1,7 @@
 export default {
 
     oidc: {
-        clientId: '0oa3aex4tOs2hZpqo5d6',
+        clientId: '0oa3x3a7bIjnw57LN5d6',
         issuer: 'https://dev-7562370.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'] 
